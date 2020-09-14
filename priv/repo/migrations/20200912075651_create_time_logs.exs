e8a4635d28b61f99b11e7a2b8140e0a64b66ceef
Ben Kolera <ben.kolera@gmail.com>
2020-09-14 14:49:44 +1000

    fixing for deploy

--- priv/repo/migrations/20200912075651_create_time_logs.exs
+++ priv/repo/migrations/20200912075651_create_time_logs.exs
@@ -6,7 +6,7 @@ defmodule Untitled.Repo.Migrations.CreateTimeLogs do
       add :hours, :float
       add :notes, :string
       add :budget_line_id, references(:budget_lines, on_delete: :nothing)
-      add :budget_id, references(:budget, on_delete: :nothing)
+      add :budget_id, references(:budgets, on_delete: :nothing)
       add :day, :date
 
       timestamps()
